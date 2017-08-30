906bce66e40c56405227301595aa57d2fcdddd65
Brandon Stewart <boveus@gmail.com>
2017-08-30 17:25:03 -0600

    Add some activerecord to sightings.

--- app/models/alien_sighting.rb
+++ app/models/alien_sighting.rb
@@ -3,4 +3,25 @@ class AlienSighting < ApplicationRecord
   def location
     "#{city}, #{state}, #{country}"
   end
+
+  def self.sort_by_duration
+    order("duration_in_seconds DESC")
+  end
+
+  def self.longest
+    sort_by_duration.first
+  end
+
+  def self.shortest
+    sort_by_duration.last
+  end
+
+  def self.count_by_shape
+    group(:shape).count
+  end
+
+  def self.shape_by_country(shape)
+    where(shape: shape)
+    .group(:country).count
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'main#index'
+  #change this to a get to and manually write it in.
   scope controller: :main do
     get :dashboard
     get :map
