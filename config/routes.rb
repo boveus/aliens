a59c4348ac2d49c873ec5a856ab95febf34e7fb8
Brandon Stewart <boveus@gmail.com>
2017-08-29 22:11:05 -0600

    Add initial dashboard view with skeleton content.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'main#index'
+  scope controller: :main do
+    get :dashboard
+    get :map
+  end
   resources :alien_sightings, only: [:index, :show]
 end
