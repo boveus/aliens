e543e396a02dc94657a4215dc03f3cce876ce069
Brandon Stewart <boveus@gmail.com>
2017-08-30 18:39:52 -0600

    Add more active record.

--- app/models/alien_sighting.rb
+++ app/models/alien_sighting.rb
@@ -9,19 +9,47 @@ class AlienSighting < ApplicationRecord
   end
 
   def self.longest
-    sort_by_duration.first
+    sort_by_duration
+    .first
   end
 
   def self.shortest
-    sort_by_duration.last
+    sort_by_duration
+    .last
   end
 
   def self.count_by_shape
-    group(:shape).count
+    group(:shape)
+    .count
   end
 
   def self.shape_by_country(shape)
     where(shape: shape)
-    .group(:country).count
+    .group(:country)
+    .count
+  end
+
+  def self.count_by_country
+    group(:country).count
+  end
+
+  def self.count_by_cities(country = 'us', number = 10)
+    where(country: country)
+    .group(:city)
+    .order("count(city) DESC")
+    .count(:id)
+    .first(number)
+  end
+
+  def self.count_by_state(number = 10)
+    group(:state)
+    .order("count(state) DESC")
+    .count(:id)
+    .first(number)
+  end
+
+  def self.count_by_year
+    # select(date_trunc('year', date_posted))
+    # .group_by(date_posted)
   end
 end
