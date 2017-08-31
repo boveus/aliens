e8c3f26d5bb144deb65d090c9712bb554e8aa85d
Brandon Stewart <boveus@gmail.com>
2017-08-30 22:04:53 -0600

    Add handler for static routes / clean up dashboard.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,12 +3,27 @@ include ChartHandler
   def index
   end
 
-  def dashboard
-    @country_chart = country_chart
-    @city_chart = city_chart
-    @state_chart = state_chart
+  def shapes
     @shape_chart = shape_chart
+  end
+
+  def countries
+    @country_chart = country_chart
+  end
+
+  def cities
+    @city_chart = city_chart
+  end
+
+  def states
+    @state_chart = state_chart
+  end
+
+  def years
     @year_chart = year_chart
+  end
+
+  def dashboard
   end
 
   def map
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'main#index'
   #change this to a get to and manually write it in.
+  get 'dashboard/shapes', :to => 'main#shapes', :as => 'shapes'
+  get 'dashboard/cities', :to => 'main#cities', :as => 'cities'
+  get 'dashboard/years', :to => 'main#years', :as => 'years'
+  get 'dashboard/states', :to => 'main#states', :as => 'states'
+  get 'dashboard/countries', :to => 'main#countries', :as => 'countries'
+
   scope controller: :main do
     get :dashboard
     get :map
