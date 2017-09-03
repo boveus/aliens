fa52b2f703ddc95f3cdbb8f08f82d3ffa42b5d1c
Brandon Stewart <boveus@gmail.com>
2017-09-03 00:33:36 -0600

    Get rid of obsolete routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'main#index'
-  #change this to a get to and manually write it in.
   get 'dashboard/shapes', :to => 'main#shapes', :as => 'shapes'
   get 'dashboard/cities', :to => 'main#cities', :as => 'cities'
   get 'dashboard/years', :to => 'main#years', :as => 'years'
@@ -11,7 +10,6 @@ Rails.application.routes.draw do
   get 'dashboard/percapitabystate', :to => 'main#per_capita_by_state', :as => 'per_capita_by_state'
   get '/dashboard', :to => 'main#dashboard', :as => 'dashboard'
   get '/total_sightings_map', :to => 'main#total_sightings_map', :as => 'total_sightings_map'
-get '/per_capita_map', :to => 'main#per_capita_map', :as => 'per_capita_map'
+  get '/per_capita_map', :to => 'main#per_capita_map', :as => 'per_capita_map'
   get '/comments', :to => 'main#comments', :as => 'comments'
-  resources :alien_sightings, only: [:index, :show]
 end
